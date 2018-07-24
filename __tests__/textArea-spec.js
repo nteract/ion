96b774ff5093ffe5e1ec2a9a1c0ffe8cb6179048
alexandercbooth <alexander.c.booth@gmail.com>
2018-07-24 00:34:50 -0400

    test: updating snaps

--- __tests__/textArea-spec.js
+++ __tests__/textArea-spec.js
@@ -2,11 +2,13 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
-import { TextArea } from "../src";
+import { TextArea, Intent } from "../src";
 
 describe("TextArea", () => {
   it("renders a simple text area", () => {
-    const component = shallow(<TextArea fill={true} large={true} />);
+    const component = shallow(
+      <TextArea fill={true} large={true} intent={Intent.primary} />
+    );
     expect(toJson(component)).toMatchSnapshot();
   });
 });
