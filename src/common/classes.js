63623ba73d73f3780e799d76f22afe403f477f0c
alexandercbooth <alexander.c.booth@gmail.com>
2018-07-23 23:37:32 -0400

    chore: use classes and classNames package

--- src/common/classes.js
+++ src/common/classes.js
@@ -1,7 +1,12 @@
 // @flow
 
 import type { ElevationLevel } from "./elevation";
+import type { IntentLevel } from "./intent";
 import { Elevation } from "./elevation";
+import { Intent } from "./intent";
+
+export const FILL = `nt-fill`;
+export const LARGE = `nt-large`;
 
 export function elevation(level: ?ElevationLevel): string {
   console.log(level);
@@ -11,4 +16,14 @@ export function elevation(level: ?ElevationLevel): string {
   }
 
   return `nt-elevation-${Elevation[level]}`;
+}
+
+export function intent(level: ?IntentLevel): string {
+  console.log(level);
+
+  if (!level || !Intent[level]) {
+    return "nt-intent-none";
+  }
+
+  return `nt-intent-${Intent[level]}`;
 }
--- src/components/textArea.js
+++ src/components/textArea.js
@@ -1,40 +1,81 @@
 // @flow
 import * as React from "react";
+import classNames from "classnames";
+import { Intent } from "../common/intent";
+import type { IntentLevel } from "../common/intent";
+
+import * as Classes from "../common/classes";
 
 type TextAreaProps = {
   fill?: boolean,
-  large?: boolean
+  large?: boolean,
+  intent?: IntentLevel
 };
 
 export class TextArea extends React.Component<TextAreaProps, null> {
   static defaultProps = {
     fill: null,
-    large: null
+    large: null,
+    intent: null
   };
 
   render() {
-    let className = "";
-    const props = this.props;
-
-    Object.keys(props).map(prop => {
-      if (props[prop]) {
-        className += " " + prop;
-      }
+    const className = classNames(Classes.intent(this.props.intent), {
+      [Classes.FILL]: this.props.fill,
+      [Classes.LARGE]: this.props.large
     });
 
     return (
       <div>
         <textarea className={className} />
         <style jsx>{`
-          .fill {
+          .nt-fill {
             display: block;
             width: 100%;
             -webkit-box-sizing: border-box;
             -moz-box-sizing: border-box;
             box-sizing: border-box;
           }
-          .large {
+          .nt-large {
             font-size: large;
+          }
+          .nt-intent-primary {
+            outline: none;
+            border: none;
+            border-radius: 3px;
+            -webkit-box-shadow: 0 0 0 0 rgba(19, 124, 189, 0),
+              0 0 0 0 rgba(19, 124, 189, 0),
+              inset 0 0 0 1px rgba(16, 22, 26, 0.15),
+              inset 0 1px 1px rgba(16, 22, 26, 0.2);
+            box-shadow: 0 0 0 0 rgba(19, 124, 189, 0),
+              0 0 0 0 rgba(19, 124, 189, 0),
+              inset 0 0 0 1px rgba(16, 22, 26, 0.15),
+              inset 0 1px 1px rgba(16, 22, 26, 0.2);
+            background: #fff;
+            height: 30px;
+            padding: 0 10px;
+            vertical-align: middle;
+            line-height: 30px;
+            color: #182026;
+            font-size: 14px;
+            font-weight: 400;
+            -webkit-transition: -webkit-box-shadow 0.1s
+              cubic-bezier(0.4, 1, 0.75, 0.9);
+            transition: -webkit-box-shadow 0.1s cubic-bezier(0.4, 1, 0.75, 0.9);
+            transition: box-shadow 0.1s cubic-bezier(0.4, 1, 0.75, 0.9);
+            transition: box-shadow 0.1s cubic-bezier(0.4, 1, 0.75, 0.9),
+              -webkit-box-shadow 0.1s cubic-bezier(0.4, 1, 0.75, 0.9);
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+            -webkit-box-shadow: 0 0 0 0 rgba(19, 124, 189, 0),
+              0 0 0 0 rgba(19, 124, 189, 0), inset 0 0 0 1px #137cbd,
+              inset 0 0 0 1px rgba(16, 22, 26, 0.15),
+              inset 0 1px 1px rgba(16, 22, 26, 0.2);
+            box-shadow: 0 0 0 0 rgba(19, 124, 189, 0),
+              0 0 0 0 rgba(19, 124, 189, 0), inset 0 0 0 1px #137cbd,
+              inset 0 0 0 1px rgba(16, 22, 26, 0.15),
+              inset 0 1px 1px rgba(16, 22, 26, 0.2);
           }
         `}</style>
       </div>
